0c8e57c339cefad02fdf453d86d2e305112e2774
bharathUIUX <dbharath@socialcommerceventures.com>
2018-09-21 20:21:07 +0530

    Chats-UI-UX-Legend-Fix

--- src/app/components/dashboard/dashboard-jobsview/dashboard-jobsview.component.ts
+++ src/app/components/dashboard/dashboard-jobsview/dashboard-jobsview.component.ts
@@ -16,38 +16,38 @@ export class DashboardJobsviewComponent implements OnInit {
     @Input() applicantStatistics: ApplicantStatistics;
     constructor(private route: ActivatedRoute) { }
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [10, 20, 30, 40, 45, 50, 55], label: 'Series A'},
+    {data: [0, 0, 0, 2, 0, 0, 0], label: 'Series B'},
+    {data: [0, 0, 0, 1, 0, 0, 0], label: 'Series C'}
   ];
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
     responsive: true
   };
   public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(235,231,253,0.2)',
-      borderColor: 'rgba(235,231,253,1)',
-      pointBackgroundColor: 'rgba(235,231,253,1)',
+    { //  job posted
+      backgroundColor: 'rgba(172,154,249,0.2)',
+      borderColor: 'rgba(172,154,249,1)',
+      pointBackgroundColor: 'rgba(172,154,249,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(235,231,253,0.8)'
+      pointHoverBorderColor: 'rgba(172,154,249,0.8)'
     },
-    { // dark grey
-      backgroundColor: 'rgba(167,217,117,0.2)',
-      borderColor: 'rgba(167,217,117,1)',
-      pointBackgroundColor: 'rgba(167,217,117,1)',
+    { // job filled
+      backgroundColor: 'rgba(132,222,203,0.2)',
+      borderColor: 'rgba(132,222,203,1)',
+      pointBackgroundColor: 'rgba(132,222,203,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(167,217,117,1)'
+      pointHoverBorderColor: 'rgba(132,222,203,1)'
     },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
+    { //  jobs cancelled
+      backgroundColor: 'rgba(167,217,217,0.2)',
+      borderColor: 'rgba(167,217,217,1)',
+      pointBackgroundColor: 'rgba(167,217,217,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      pointHoverBorderColor: 'rgba(167,217,217,0.8)'
     }
   ];
   public lineChartLegend:boolean = true;
