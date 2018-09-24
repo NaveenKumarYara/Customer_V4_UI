dfc147bff5bdd5cbfc069e656df87b550b33a3ec
ManiPriya <manipriya@socialcommerceventures.com>
2018-09-24 11:07:48 +0530

    fixed dashboard loader

--- src/app/components/dashboard/dashboard-recentjobs/dashboard-recentjobs.component.ts
+++ src/app/components/dashboard/dashboard-recentjobs/dashboard-recentjobs.component.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RecentJobs } from '../../../../models/recentjobs';
-
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DashboardService } from '../dashboard.service';
 @Component({
   selector: 'app-dashboard-recentjobs',
   templateUrl: './dashboard-recentjobs.component.html',
-  styleUrls: ['./dashboard-recentjobs.component.css']
+  styleUrls: ['./dashboard-recentjobs.component.css'],
+  providers: [NgxSpinnerService]
 })
 export class DashboardRecentjobsComponent implements OnInit {
-    @Input() recentjoblist: RecentJobs;
-
-    constructor() { }
+    //@Input() recentjoblist: RecentJobs;
+    recentjoblist: RecentJobs[] = [];
+   jobLoader =false;
+    color = 'primary';
+    mode = 'indeterminate';
+    value = 50;
+    
+    // progressValue = 60;
+    // isDeterminate = true;
+  
+    // step(val: number) {
+    //   this.progressValue = Math.max(0, Math.min(100, val + this.progressValue));
+    // }
+    constructor(private spinner: NgxSpinnerService, private dashboardservice: DashboardService) { }
 
 
     
 
     ngOnInit() {
+      this.jobLoader=true;
+      this.populateRecentJoblist(5);
+     // this.spinner.show();
+    //  let ldg = $('#domainLoader');
+    //  ldg.find('> div > span').text('Please wait as jobs are loading')
+    //    .end().show();
+    //   setTimeout(() => {
+    //     /** spinner ends after 5 seconds */
+    //     ldg.hide();
+    //   }, 4000);
   }
+  populateRecentJoblist(count: number) {
+    return this.dashboardservice.getRecentJobs(count).subscribe(res => {
+        this.recentjoblist = res;
+        this.jobLoader=false;
+    });
+}
 
 }
