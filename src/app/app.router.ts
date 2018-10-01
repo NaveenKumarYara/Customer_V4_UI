2b1e898e196efa873bd406cbec9ede12eb528caf
Naveenyara <naveen@socialcommerceventures.com>
2018-10-01 13:05:45 +0530

    removed the hash in url

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -173,4 +173,4 @@ const appRoutes: Routes =
     { path: '**', component: HomeComponent }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true});
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: false});
