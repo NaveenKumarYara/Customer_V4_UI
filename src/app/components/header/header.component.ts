ec88df6f29c19343722743e268cdc387ac26cbc5
Naveenyara <naveen@socialcommerceventures.com>
2018-10-04 16:11:40 +0530

    Clear the Logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'   
 })
 export class HeaderComponent {  
   appTitle = 'Tenendus';
+  constructor( private router: Router) {
+
+  }
+  Logout() {
+    sessionStorage.clear();
+    this.router.navigateByUrl('/home');
+}
+ngInit()
+{
+  
+}
 }
