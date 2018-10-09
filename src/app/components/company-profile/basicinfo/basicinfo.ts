7008ffc5184c4fc4244f776fb5d894bb9020653e
Naveenyara <naveen@socialcommerceventures.com>
2018-10-09 14:49:55 +0530

    Added the basic info Edit and add option

---
+++ src/app/components/company-profile/basicinfo/basicinfo.ts
@@ -0,0 +1,14 @@
+export class basicinfo
+{
+    customerId: number;
+    companyName: string;
+    website: string;
+    contactEmail: string;
+    twitterURL: string;
+    linkedInURL: string;
+    facebookURL: string;
+    mobilePhone: string;
+    homePhone: string;
+    address1: string;
+    address2: string
+}
