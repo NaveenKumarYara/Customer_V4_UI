94d771184562b42d5e30f6ca7a2abbaffdb14078
Srilakshmi Gogineni <srilakshmi@socialcommerceventures.com>
2018-10-16 14:43:17 +0530

    Added Password show option

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -51,6 +51,16 @@ export class HomeComponent {
       'UserName': ['', Validators.compose([Validators.required])],
       'Password': ['', Validators.compose([Validators.required])],
     });
+    $(".glyphicon-eye-open").on("click", function () {
+      $(this).toggleClass("glyphicon-eye-close");
+      var type = $("#password").attr("type");
+      if (type == "text") {
+        $("#password").prop('type', 'password');
+      }
+      else {
+        $("#password").prop('type', 'text');
+      }
+    });
 
   }
 }
