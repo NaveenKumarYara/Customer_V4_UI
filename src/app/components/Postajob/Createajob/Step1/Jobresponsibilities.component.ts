a562ebc190347239b425f37ead18f536f424d171
ManiPriya <manipriya@socialcommerceventures.com>
2018-11-09 17:51:17 +0530

    fixed roles

--- src/app/components/Postajob/Createajob/Step1/Jobresponsibilities.component.ts
+++ src/app/components/Postajob/Createajob/Step1/Jobresponsibilities.component.ts
@@ -42,6 +42,7 @@ export class JobResponsibilitiesComponent implements OnInit, OnDestroy {
   private deleteResponsibility(index: number) {
     this.appService.deleteResponsibilities(index);
     this.roleList.splice(index, 1);
+     this.roleIdList.splice(index, 1);
   }
 
   ngOnInit() {
