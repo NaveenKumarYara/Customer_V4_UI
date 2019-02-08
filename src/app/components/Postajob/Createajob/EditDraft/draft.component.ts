1604824d2e501ef8c9e98a6116943c28d4d99566
Naveenyara <naveen@socialcommerceventures.com>
2019-02-08 16:49:14 +0530

    Added the Drafts and edit

--- src/app/components/Postajob/Createajob/EditDraft/draft.component.ts
+++ src/app/components/Postajob/Createajob/EditDraft/draft.component.ts
@@ -1,21 +1,53 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import{draftDetails} from '../../../../../models/draftDetails';
+import { AppService } from '../../../../app.service';
 declare var $: any; 
 @Component({
   
   selector: 'app-editdraft',
-  templateUrl: './draft.component.html'
+  templateUrl: './draft.component.html',
+  providers: [AppService]
 })
 export class EditDraftComponent {
-
-  constructor( private fb: FormBuilder, private router: Router) {
-
+  customer: any;
+  customerId: any;
+  complete:any;
+  userId: any;
+  drafts: draftDetails[];
+  constructor( private fb: FormBuilder, private router: Router,private appService: AppService) {
+    this.customer = JSON.parse(sessionStorage.getItem('userData'));
+    this.customerId = this.customer.CustomerId;
+    this.userId = this.customer.UserId;
   }
  
+  editJob(jobId,active) {
+
+      this.complete = 4;
+      this.router.navigate(['/app-createajob/', {jobId} ]);
+      localStorage.setItem('completed', JSON.stringify(this.complete));
+      this.router.navigate(['/app-createajob/app-steps-step1/', {jobId} ]);
+    
+
+      // this.router.navigateByUrl('/app-createajob/app-steps-step1/id='+ jobId);
+   // [routerLink]="['/app-createajob/app-steps-step1/',job.JobId]"
+  }
+
+  back()
+  {
+    this.router.navigateByUrl('/app-postajob');
+  }
+
+  GetEditDrafts()
+  {
+    return this.appService.GetEditDrafts(this.customerId,this.userId).subscribe(res => {
+      this.drafts = res;
+    })
+  }
 
   ngOnInit() {
-  
+  this.GetEditDrafts();
   }
 }
 
