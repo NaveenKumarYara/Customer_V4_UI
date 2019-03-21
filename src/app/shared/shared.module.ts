5f07ac0a9a58f655232d0d5ae52557eac8e30d4f
ManiPriya <manipriya.chakravartula@arytic.com>
2019-03-21 17:06:09 +0530

    misc

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LogoHeaderComponent } from './logoheader/logoheader.component';
 import { AgmCoreModule } from '@agm/core';
 import { routing } from '../app.router';
-import{AlertComponent} from './alerts/alerts.component'
+import {AlertComponent} from './alerts/alerts.component';
 import { AppService } from '../app.service';
 import { AlertService} from './alerts/alerts.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,8 +13,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     routing,
    FormsModule, ReactiveFormsModule
   ],
-  providers: [AppService,AlertService],
-  declarations: [LogoHeaderComponent,AlertComponent],
-  exports: [LogoHeaderComponent,AlertComponent]
+  providers: [AppService, AlertService],
+  declarations: [LogoHeaderComponent, AlertComponent],
+  exports: [LogoHeaderComponent, AlertComponent]
 })
 export class SharedModule { }
