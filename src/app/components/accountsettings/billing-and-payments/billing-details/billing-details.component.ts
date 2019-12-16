2c3275ace83b1e5b9218d970e32802307822e881
baleshward <baleshwar.dabbikar@outlook.com>
2019-12-16 16:39:59 +0530

    Billing-Details-UI

--- src/app/components/accountsettings/billing-and-payments/billing-details/billing-details.component.ts
+++ src/app/components/accountsettings/billing-and-payments/billing-details/billing-details.component.ts
@@ -4,7 +4,8 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CompanyProfile } from '../../../../../models/companyprofile';
 import { AppService } from '../../../../app.service';
-
+declare var jQuery:any;
+declare var $:any;
 @Component({
   selector: 'app-billing-details',
   templateUrl: './billing-details.component.html',
@@ -19,6 +20,31 @@ export class BillingDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.populateCompanyProfile(); 
+    (function ($) {
+
+      var $editBtn = $('.edit-btn');
+    
+      $.each($editBtn, function (index, element) {
+        var $element = $(element);
+        $element.on('click', function (e) {
+          e.preventDefault();
+          var $editGroup = $element.closest('.edit-group');
+          var $editForm = $editGroup.find('.edit-form');
+          var $data = $editGroup.find('.data');
+          var $cancelBtn = $editForm.find('.btn-border');
+    
+          $data.hide();
+          $editForm.show();
+    
+          $cancelBtn.on('click', function (e) {
+            e.preventDefault();
+            $data.show();
+            $editForm.hide();
+          });
+        });
+      });
+    })(jQuery);
+
   }
 
   populateCompanyProfile() {
