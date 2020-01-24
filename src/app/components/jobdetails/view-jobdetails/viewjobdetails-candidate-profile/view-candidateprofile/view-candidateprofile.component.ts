3af31eb0e04e31ad699939af4208ab0a57de8d0a
Aniket Shahane <aniket.shahane@prosot.com>
2020-01-24 16:24:34 +0530

    QATC-488 - issue fix  private profiles test report

--- src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/view-candidateprofile/view-candidateprofile.component.ts
+++ src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/view-candidateprofile/view-candidateprofile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject , ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { ApiService } from '../../../../../shared/services/api.service/api.service';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -23,12 +23,12 @@ export class ViewCandidateprofileComponent implements OnInit {
   customer: any;
   customerId: any;
   userId: any;
-  email:any;
+  email: any;
   profileview: any;
   profileId: any;
   details: mappingdetails;
   list: any;
-  noTest:boolean=false;
+  noTest: boolean = false;
   otherSkills: any = [];
   options: CloudOptions = {
     // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value
@@ -36,7 +36,7 @@ export class ViewCandidateprofileComponent implements OnInit {
     height: 400,
     overflow: false,
     realignOnResize: true,
-  }; 
+  };
   graphData: any[] = [];
   graphLabel: any[] = [];
   graphLabelList: LegendList[] = [];
@@ -59,9 +59,10 @@ export class ViewCandidateprofileComponent implements OnInit {
 
   chartLabels = ['Openess to Experience', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'Neuroticism'];
   private doughnutChartColors: any[] = [{ backgroundColor: ["#6569A9", "#3FB8B3", "#EC8885", "#666666", "#64A489"] }];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private _service: ApiService, private router: Router,private jobdetailsservice: JobdetailsService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+    private _service: ApiService, private router: Router, private jobdetailsservice: JobdetailsService) {
     //this.preId = sessionStorage.getItem('Preid');
-    this.noTest=false;
+    this.noTest = false;
     this.profileId = JSON.parse(sessionStorage.getItem('Preid'));
   }
   /*dashboard graph*/
@@ -126,15 +127,20 @@ export class ViewCandidateprofileComponent implements OnInit {
     this.GetCandidateSKills();
     this.GetProfileDetails();
     this.GetUserProfileInfo();
-    this.GetCandidatePersonalityResult();
+    this._service.GetService('ProfileAPI/api/GetProfileStatus?profileId=', this.data.ProfileId).subscribe(
+      data => {
+        this.noTest = data;
+      });
+    if (this.noTest) {
+      this.GetCandidatePersonalityResult();
+    }
   }
 
   onChartClick(event) {
     console.log(event);
   }
 
-  GetProfileDetails()
-  {
+  GetProfileDetails() {
     this._service.GetService('JobsAPI/api/GetUserInfoByProfileMapping?profileId=', this.data.ProfileId).subscribe(
       data => {
         this.details = data;
@@ -148,82 +154,79 @@ export class ViewCandidateprofileComponent implements OnInit {
       })
   }
 
-  GetCandidatePersonalityResult()
-  {
-    this._service.GetService('ProfileAPI/api/GetProfileEmail?profileId=',this.data.ProfileId).subscribe(
-    email => {
-    this.email=email.UserName;
-    this.jobdetailsservice.getPersonalityTest(this.email).subscribe(
-      data => {
-        if(data.length>0)
-        {
-        this.graphData = [];
-        this.graphLabel = [];
-        var userResponse = data;
-        this.graphLabelList = [];
+  GetCandidatePersonalityResult() {
+    this._service.GetService('ProfileAPI/api/GetProfileEmail?profileId=', this.data.ProfileId).subscribe(
+      email => {
+        this.email = email.UserName;
+        this.jobdetailsservice.getPersonalityTest(this.email).subscribe(
+          data => {
+            if (data.length > 0) {
+              this.graphData = [];
+              this.graphLabel = [];
+              var userResponse = data;
+              this.graphLabelList = [];
 
-        var count = 0;
-        if (this.testChart) {
-          var testChartCanvas = this.testChart.nativeElement.getContext('2d');
-          userResponse.forEach((a, index) => {
-            this.graphData.push(a.response.toFixed(2));
-            this.graphLabel.push(a.groupName);
-            this.graphLabelList.push(new LegendList());
-            this.graphLabelList[index].GroupLabel = (a.groupName);
-            this.graphLabelList[index].GroupPer = (a.response.toFixed(2));
-            this.noTest=true;
-          });
+              var count = 0;
+              if (this.testChart) {
+                var testChartCanvas = this.testChart.nativeElement.getContext('2d');
+                userResponse.forEach((a, index) => {
+                  this.graphData.push(a.response.toFixed(2));
+                  this.graphLabel.push(a.groupName);
+                  this.graphLabelList.push(new LegendList());
+                  this.graphLabelList[index].GroupLabel = (a.groupName);
+                  this.graphLabelList[index].GroupPer = (a.response.toFixed(2));
+                  this.noTest = true;
+                });
 
-          this.graphLabelList[0].GroupColor = ('rgba(101,105, 169, 1)');
-          this.graphLabelList[1].GroupColor = ('rgba(63, 184, 179, 1)');
-          this.graphLabelList[2].GroupColor = ('rgba(236, 136, 133, 1)');
-          this.graphLabelList[3].GroupColor = ('rgba(235, 189, 78, 1)');
-          this.graphLabelList[4].GroupColor = ('rgba(100, 164, 137, 1)');
+                this.graphLabelList[0].GroupColor = ('rgba(101,105, 169, 1)');
+                this.graphLabelList[1].GroupColor = ('rgba(63, 184, 179, 1)');
+                this.graphLabelList[2].GroupColor = ('rgba(236, 136, 133, 1)');
+                this.graphLabelList[3].GroupColor = ('rgba(235, 189, 78, 1)');
+                this.graphLabelList[4].GroupColor = ('rgba(100, 164, 137, 1)');
 
-          var weekChart = new Chart(testChartCanvas, {
-            type: 'doughnut',
-            options: {
-              title: {
-                display: true,
-                text: "Personality Test Chart"
-              },
-              legend: {
-                display: false,
-              },
-            },
-            data: {
-              labels: this.graphLabel,
-              render: 'labels',
-              datasets: [{
-                labels: [
-                  'Red',
-                  'Yellow',
-                  'Blue',
-                  'pink',
-                  'black'
-                ],
-                label: '# of Votes',
-                data: this.graphData,
-                backgroundColor: [
-                  'rgba(101,105, 169, 1)',
-                  'rgba(63, 184, 179, 1)',
-                  'rgba(236, 136, 133, 1)',
-                  'rgba(235, 189, 78, 1)',
-                  'rgba(100, 164, 137, 1)'
-                ],
+                var weekChart = new Chart(testChartCanvas, {
+                  type: 'doughnut',
+                  options: {
+                    title: {
+                      display: true,
+                      text: "Personality Test Chart"
+                    },
+                    legend: {
+                      display: false,
+                    },
+                  },
+                  data: {
+                    labels: this.graphLabel,
+                    render: 'labels',
+                    datasets: [{
+                      labels: [
+                        'Red',
+                        'Yellow',
+                        'Blue',
+                        'pink',
+                        'black'
+                      ],
+                      label: '# of Votes',
+                      data: this.graphData,
+                      backgroundColor: [
+                        'rgba(101,105, 169, 1)',
+                        'rgba(63, 184, 179, 1)',
+                        'rgba(236, 136, 133, 1)',
+                        'rgba(235, 189, 78, 1)',
+                        'rgba(100, 164, 137, 1)'
+                      ],
 
+                    }
+                    ]
+                  }
+                });
               }
-              ]
             }
-          });
-        }
-      }
-      else
-      {
-      this.testChart1;
-      }
+            else {
+              this.testChart1;
+            }
+          })
       })
-    })
   }
 
   GetUserProfileInfo() {
@@ -257,6 +260,6 @@ export class LegendList {
   GroupLabel: string;
   GroupColor: string;
   GroupPer: string;
-  groupName:any;
-  response:any;
+  groupName: any;
+  response: any;
 }
