c7b883683a71103c4769a1608832298007c0e31c
NaveenkumarYara <naveen@socialcommerceventures.com>
2020-06-01 15:41:11 +0530

    Prod changes

--- src/app/components/accountsettings/billing-and-payments/billing-details/billing-details.component.ts
+++ src/app/components/accountsettings/billing-and-payments/billing-details/billing-details.component.ts
@@ -33,10 +33,10 @@ export class BillingDetailsComponent implements OnInit {
     this.populateCompanyProfile(); 
     this.GetCustomerSubscription();
     window['Chargebee'].init({
-       site: 'arytic-test',
-       publishableKey: 'test_LA9gcddwXA2XIgAkHzgs2FuQsewoId4we'
-      // site: 'arytic',
-      // publishableKey: 'live_NMr0XTWcusb8hdRcdvF1Du9shtmawgjvyA'
+       //site: 'arytic-test',
+       //publishableKey: 'test_LA9gcddwXA2XIgAkHzgs2FuQsewoId4we'
+      site: 'arytic',
+       publishableKey: 'live_NMr0XTWcusb8hdRcdvF1Du9shtmawgjvyA'
     });
   }
 
