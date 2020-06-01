9eff231ef279611748d415c8c5a44f3d0030ddaf
NaveenkumarYara <naveen@socialcommerceventures.com>
2020-06-01 14:42:56 +0530

    Fixed the Suggested Count

--- src/app/components/dashboard/dashboard-recentjobs/recentjobscount/recentjobscount.component.ts
+++ src/app/components/dashboard/dashboard-recentjobs/recentjobscount/recentjobscount.component.ts
@@ -45,7 +45,10 @@ export class RecentjobsCountComponent implements OnInit {
   {
   this.jobId = this.job.JobId;
     return this.dashboardservice.getSuggestedCount(this.jobId).subscribe(res => {
-    this.jobS = res;
+      setInterval(() => {
+        this.jobS = res;
+      }, 1000);
+    //this.jobS = res;
    });
   }
 
