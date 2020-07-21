584db79e45d43af4404b1b33c2913f6507d258fd
NaveenkumarYara <naveen@socialcommerceventures.com>
2020-07-21 11:50:56 +0530

    Changes for hire

--- src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/Hiringdialog/hire.component.ts
+++ src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/Hiringdialog/hire.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, Input, Output, EventEmitter,ViewContainerRef} from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { ScheduleInterview } from '../schedule-interview/schedule-interview.component';
 import { JobdetailsService } from '../../../jobdetails.service';
 import {ToastsManager, Toast} from 'ng2-toastr/ng2-toastr';
 import { AppService } from '../../../../../app.service';
 import {  NgbModal, NgbModule, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from '../../../../../shared/services/api.service/api.service';
+import { IfObservable } from 'rxjs/observable/IfObservable';
 declare var $: any;
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
@@ -26,12 +27,13 @@ export class HiredialogComponent {
   addon = new addon();
   valueSal:number;
   IDate:any;
+  contract:string;
   TypeId:any;
   schIntw = new ScheduleInterview();
  @Input() jobid: number;
  @Input() statusid: number;
  @Output() eventStat = new EventEmitter();
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private _service: ApiService,private appService: AppService,private jobdetailsservice: JobdetailsService,private toastr: ToastsManager, private _vcr: ViewContainerRef) {
+  constructor(public dialogRef: MatDialogRef<HiredialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any,private _service: ApiService,private appService: AppService,private jobdetailsservice: JobdetailsService,private toastr: ToastsManager, private _vcr: ViewContainerRef) {
     this.customer = JSON.parse(sessionStorage.getItem('userData'));
     this.customerId = JSON.parse(sessionStorage.getItem('customerId'));
       this.userId = this.customer.UserId;
@@ -71,13 +73,57 @@ Check()
   this.addon.AddonId = "2";
   if (this.salaryDetails.SalaryTypeId == 1)
   {
+    if(this.TypeId == 2 && this.contract != null)
+    {
+      if(this.contract == "0-3 months")
+      {
+        var val1 = ((Math.round(this.valueSal)*2000*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(val1)/4;
+      }
+      else if(this.contract == "3-6 months")
+      {
+        var val2 = ((Math.round(this.valueSal)*2000*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(val2)/2;
+      }
+      else if(this.contract >= "6-12 months")
+      {
+        var val3 = ((Math.round(this.valueSal)*2000*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(val3);
+      }
+    }
+    else if(this.TypeId != 2)
+     {
     var val = ((Math.round(this.valueSal)*2000*7)).toFixed(0);
     this.addon.AddonUnitPrice = Number(val);
+     }
+ 
   }
   else if (this.salaryDetails.SalaryTypeId == 2)
   {
-    var value = ((Math.round(this.valueSal)*7)).toFixed(0);
-    this.addon.AddonUnitPrice = Number(value);
+    if(this.TypeId == 2 && this.contract != null)
+    {
+      if(this.contract == "0-3 months")
+      {
+        var value1 = ((Math.round(this.valueSal)*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(value1)/4;
+      }
+      else if(this.contract == "3-6 months")
+      {
+        var value2 = ((Math.round(this.valueSal)*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(value2)/2;
+      }
+      else if(this.contract >= "6-12 months")
+      {
+        var value3 = ((Math.round(this.valueSal)*7)).toFixed(0);
+        this.addon.AddonUnitPrice = Number(value3);
+      }
+    }
+    else if(this.TypeId != 2)
+    {
+      var value = ((Math.round(this.valueSal)*7)).toFixed(0);
+      this.addon.AddonUnitPrice = Number(value);
+    }
+   
   }
   this.addon.AddonQuantity = 1;
    return this.jobdetailsservice.AddonHirefee(this.addon).subscribe(result => {
@@ -103,6 +149,7 @@ gotit(na) {
         this.salaryDetails = data;
         this.valueSal=this.salaryDetails.MaximumSalary;
         this.TypeId = this.salaryDetails.EmploymentTypeId;
+        this.contract = this.salaryDetails.ContractDuration;
       });
    }
 
@@ -145,6 +192,7 @@ gotit(na) {
     // this.jobDetails.populateJobsStaticInfo(this.jobid);
       this.Check();
       this.eventStat.emit(null);
+      this.dialogRef.close();
       console.log(res);
       }) ;
     }
