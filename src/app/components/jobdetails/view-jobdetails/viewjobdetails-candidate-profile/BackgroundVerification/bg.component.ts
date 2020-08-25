abe1de516ef6bb195cfa96813605cf27261fa88c
yaranaveenkumar <yaranaveenkumar@outlook.com>
2020-08-25 17:27:28 +0530

    Changes for Bg Verification

--- src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/BackgroundVerification/bg.component.ts
+++ src/app/components/jobdetails/view-jobdetails/viewjobdetails-candidate-profile/BackgroundVerification/bg.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Inject, Input, Output, EventEmitter,ViewContainerRef } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { ScheduleInterview } from '../schedule-interview/schedule-interview.component';
 import { JobdetailsService } from '../../../jobdetails.service';
+import {ToastsManager, Toast} from 'ng2-toastr/ng2-toastr';
+import { AppService } from '../../../../../app.service';
 import { ApiService } from '../../../../../shared/services/api.service/api.service';
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
@@ -14,6 +16,9 @@ export interface DialogData {
 export class backgrounddialogComponent {
     Bglist: any=[];
     Dlist: any=[];
+    addon = new addon();
+    loading : boolean=false;
+    Verification : BackgroundVerification;
     bgverification = new BackgroundVerification();
     showRes : boolean = false;
     val1 = 0;
@@ -22,17 +27,20 @@ export class backgrounddialogComponent {
     val4 = 0;
     name:any;
     subtotal : string;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private _service: ApiService, private jobdetailsservice: JobdetailsService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private appService: AppService,private _service: ApiService, private jobdetailsservice: JobdetailsService,private toastr: ToastsManager, private _vcr: ViewContainerRef) {
      this.GetBG();
      this.name = this.data.Name;
-     debugger
+     this.toastr.setRootViewContainerRef(_vcr);
      this.GetDrugVerification();
+     this.GetBGTestResult()
    }
 
   GetBG()
   {
+    this.loading= true;
     this._service.GetService('IdentityAPI/api/GetBackgroundList', '').subscribe(
         data => {
+          this.loading =false;
             this.Bglist=data;
         }
     )
@@ -47,18 +55,63 @@ export class backgrounddialogComponent {
   )
   }
 
+  GetBGTestResult()
+  {
+    this._service.GetService('IdentityAPI/api/GetCandidateBackgroundVerification?profileId=' + this.data.ProfileId, '&customerUserId=' + this.data.CuserId).subscribe(
+      data => {
+          this.Verification = data[0];
+          if(this.Verification.ProfileId>0)
+          {
+            this.bgverification.CriminalOptionSelected = this.Verification.CriminalOptionSelected;
+            this.bgverification.DrugOptionSelected = this.Verification.DrugOptionSelected;
+            this.bgverification.Education = this.Verification.Education;
+            this.bgverification.Employment = this.Verification.Employment;
+            this.subtotal = this.Verification.Price;
+            this.bgverification.Certification = this.bgverification.Certification;
+            this.bgverification.Reference = this.bgverification.Reference;
+            this.bgverification.CriminalOption = this.bgverification.CriminalOption;
+            this.bgverification.DrugTest = this.bgverification.DrugTest;
+          }
+          else
+          {
+            this.Verification = new BackgroundVerification();
+          }
+      }
+  )
+  }
+
+  SaveBgVerIfication()
+  {
+    return this._service.PostService(this.bgverification, 'IdentityAPI/api/SaveBackGroundVerification')
+    .subscribe(data => {
+      if(data >=0)
+      {
+      this.toastr.success('Processing Request', 'Success');
+      return this.appService.GetCustomerSubscription(this.data.CuserId).subscribe(res => {
+      if(res.subscriptionId!=undefined)
+      {
+      this.addon.SubscriptionId = res.subscriptionId;
+      this.addon.AddonId = "2";
+      this.addon.AddonUnitPrice = Number(this.subtotal);
+      this.addon.AddonQuantity = 1;
+      return this.jobdetailsservice.AddonHirefee(this.addon).subscribe(result => {
+        console.log(result);
+        this.toastr.success('Mail Sent', 'Success');
+        this.GetBGTestResult();
+
+      });
+     
+    }
+    });
+  }
+  });
+  }
+
 
 
   SaveBg(val)
   {
-    if(val ==1)
-    {
-      this.showRes = false;
-    }
-    else
-    {
-      this.showRes = true; 
-    }
+ 
 
     if(this.bgverification.CriminalOptionSelected>0)
     {
@@ -95,22 +148,39 @@ export class backgrounddialogComponent {
     if(this.bgverification.Certification === false)
     {
       this.val4 = 0;
-    }
-   
+    }   
    const total = (this.val1+this.val2+this.val3+this.val4).toFixed(2);
    this.subtotal = total.toString();
    this.bgverification.Price = this.subtotal;
    this.bgverification.ProfileId = this.data.ProfileId;
    this.bgverification.CustomerUserId = this.data.CuserId;
-   this.bgverification;
-  debugger
+   if(val == 1)
+   {
+     this.showRes = false;
+   }
+   else
+   {
+     if(this.bgverification.DrugTest == null || this.bgverification.DrugOptionSelected==0)
+     {
+       this.toastr.warning('Please Choose Drug Test','Oops');
+        return false;
+     }
+     else
+     {
+     this.showRes = true; 
+     if(val == 2)
+     {
+       this.SaveBgVerIfication();
+     }
+    }
+   }
+  
   }
  
 }
 
 export class BackgroundVerification
 {
-
   CustomerUserId: number;
   ProfileId: number;
   CriminalOption:boolean; 
@@ -122,4 +192,12 @@ export class BackgroundVerification
   Certification: boolean;
   Reference: boolean;
   Price: string;
+}
+
+export class addon
+{
+    SubscriptionId: string;
+    AddonId:string;
+    AddonUnitPrice:number;
+    AddonQuantity:number;
 }
