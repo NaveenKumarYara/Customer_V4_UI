842555092df984517d177e9fd8b5240e22701aae
Seshu Kumar R. S. R <seshukumar.rsr@hotmail.com>
2021-06-02 20:51:44 +0530

    Post a Job Initial Development.

--- src/app/components/job-posting/job-posting.component.ts
+++ src/app/components/job-posting/job-posting.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-job-posting',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobPostingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private appService: AppService, private zone: NgZone) { }
 
   ngOnInit() {
   }
 
+
+  createJob() {
+    $("#activepostjob").addClass('active');
+    let activepostjob = true;
+    localStorage.setItem('activepostjob', JSON.stringify(activepostjob));
+    //this.router.navigateByUrl('/app-step1');
+    //this.router.navigate(['/app-step1'])
+    this.router.navigate(["/app-job-posting", "app-step2"]);
+    // this.location.go('/app-createajob');
+    // this.reload();
+  }
 }
