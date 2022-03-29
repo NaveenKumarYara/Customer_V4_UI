e23a817b9bd48d8c5c3db2664caf3a5581fac87d
Yara Naveen Kumar <yaranaveenkumar@outlook.com>
2022-03-29 13:25:10 +0530

    Added Jd Parser Basic Edition

--- src/app/components/Postajob/postajob.component.ts
+++ src/app/components/Postajob/postajob.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, Inject, NgZone  } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpEvent } from '@angular/common/http';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AppService } from '../../app.service';
+import {ToastsManager, Toast} from 'ng2-toastr/ng2-toastr';
 import {Location } from '@angular/common';
 import { Subject, Observable } from 'rxjs';
 import {EmploymentType} from '../../../models/employmenttype.model';
 import{InterviewType} from '../../../models/interviewtype.model';
 import { Jobskills } from '../../../models/jobskills.model';
 import { Qualifications } from '../../../models/qualifications.model';
-import { PjDomain, GetDomain, CustomerUsers, KeyRole,PjTechnicalTeam,Cities,jobImmigrationData, CategoryList, PjEducationDetails, PjRole, PjDisc, Roles, DiscResult, PrefLocation, ClientModel, PjDepartments, DepartmentModel, SkillPostData, GetKeyRole } from '../../components/Postajob/models/jobPostInfo';
+import { PjDomain, GetDomain, CustomerUsers, KeyRole,PjTechnicalTeam,Cities,jobImmigrationData, CategoryList, PjEducationDetails, PjRole, PjDisc, Roles, DiscResult, PrefLocation, ClientModel, PjDepartments, DepartmentModel, SkillPostData, GetKeyRole, PjSkill } from '../../components/Postajob/models/jobPostInfo';
 import { WorkAuthorization } from '../../../models/workAuthorization';
+import { ApiService } from '../../shared/services';
 declare var $: any;
 @Component({
   selector: 'app-postajob',
@@ -19,10 +22,182 @@ export class PostajobComponent implements OnInit {
 
 
 
-  constructor(private route: ActivatedRoute,
+  constructor(private route: ActivatedRoute, private toastr: ToastsManager,private _service:ApiService,
     private router: Router, private appService: AppService, private location: Location, private zone: NgZone) {
 
   }
+
+  getFileDetails(e) {
+    const fileSelected: File = e.target.files[0];
+    const stringToSplitDoc = fileSelected.name;
+    const y = stringToSplitDoc.split('.');
+    const exp = y[1];
+    if (exp === 'pdf'  || exp === 'docx'  || exp === 'doc' ) {
+      if (fileSelected.size > 2048576) {
+        // Swal('Too Big Size.. File Not Allowed');
+        this.toastr.error('Too Big Size.. File size greater than 2MB Not Allowed!');
+        setTimeout(() => {
+          this.toastr.dismissToast;
+        }, 3000);
+      }
+      else
+      {
+        this.processResumes(fileSelected);
+      }
+      
+    }
+    else 
+    {
+      this.toastr.error('Please upload the files with extension pdf,docx');
+      setTimeout(() => {
+        this.toastr.dismissToast;
+      }, 3000);    
+  }
+
+  
+  }
+
+  processResumes(file) {
+     let dta:any;
+     this.getBase64(file).then(
+      data => 
+      {
+       dta= data;
+       var da = {  
+    DocumentAsBase64String: dta,  
+    DocumentLastModified: '2021-06-18',  
+    GeocodeOptions: {  
+      'IncludeGeocoding': false,  
+      'Provider': null,  
+      'ProviderKey': null,  
+      'PostalAddress': null,  
+      'GeoCoordinates': null  
+    },  
+    IndexingOptions: {  
+      'IndexId': '',  
+      'DocumentId': '',  
+      'UserDefinedTags': [  
+        ''  
+      ]  
+    },  
+    OutputHtml: true,  
+    HideHtmlImages: null,  
+    OutputRtf: false,  
+    OutputCandidateImage: false,  
+    OutputPdf: false,  
+    Configuration: 'Coverage.MilitaryHistoryAndSecurityCredentials = true; Coverage.PatentsPublicationsAndSpeakingEvents = true; Coverage.PersonalInformation = true; Coverage.Training = true; Coverage.EntryLevel = true',  
+    SkillsData: [  
+      ''  
+    ],  
+    NormalizerData: ''  
+      }
+      this._service.UploadSovrenJob(da).subscribe( dat => 
+      {   
+        let val  = dat.Value.JobData;   
+        if(val!=null)
+        {
+          if(val.CurrentLocation!=null)
+          {
+           
+            if(val.CurrentLocation.Municipality!=null)
+            {
+              let loc = new Cities();
+              loc.CityId = 1;
+              loc.CityName= val.CurrentLocation.Municipality+","+val.CurrentLocation.Regions[0];
+              this.appService.JobLocations.push(loc);
+              this.appService.JobLocationsChanged.next(this.appService.JobLocations);
+            }
+         
+          }
+          if(val.EmployerNames!=null)
+          {
+            if(val.EmployerNames.MainEmployerName!=null)
+            {
+              let eJclient = new ClientModel();
+              eJclient.ClientId = 0;
+              eJclient.ClientName = val.EmployerNames.MainEmployerName;
+              this.appService.clientModel.next(eJclient);
+            }
+          }
+          if(val.JobTitles!=null)
+          {
+            if(val.JobTitles.MainJobTitle!=null)
+            {
+              this.appService.jobtitle.next(val.JobTitles.MainJobTitle);
+              this.appService.jobtitleId.next("1");
+            }
+          }
+          if(val.SkillsData.length>0)
+          {
+              if(val.SkillsData[0].Taxonomies[0]!=null)
+              {
+                this.appService.jobIndustry.next(val.SkillsData[0].Taxonomies[0].Name);
+                this.appService.IndustryId.next("1");                
+              }
+   
+              let ejPrimarySkills:any=[];
+               for (const ss of val.SkillsData[0].Taxonomies){
+                for (const sa of ss.SubTaxonomies)
+                {
+                  for (const skill of sa.Skills)
+                    {
+                   const ejSkills = new PjSkill();
+                   ejSkills.SkillName = skill.Name;
+                   ejSkills.SkillType = true;
+                   ejSkills.MinimumExp = 1*12;
+                   ejSkills.MaximumExp = 5*12;
+                   ejPrimarySkills.push(ejSkills);
+                  }
+                }
+               }
+               
+               if(ejPrimarySkills.length>0)
+               {
+                const ids = ejPrimarySkills.map(o => o.SkillName)
+                const filtered = ejPrimarySkills.filter(({SkillName}, index) => !ids.includes(SkillName, index + 1))
+                this.appService.primaryjobskills = filtered; 
+                this.appService.jobprimaryskillsChanged.next(this.appService.primaryjobskills ); 
+                this.appService.selectedskilltype.next('');
+               }
+          
+
+          }
+          this.appService.hasDescription.next(true);
+          this.appService.description.next(val.JobMetadata.PlainText);
+          if(val.MinimumYears!=null)
+          {
+            this.appService.minExperience.next(val.MinimumYears.Value * 12);
+          }
+
+          this.createJob();
+        }
+     
+      })
+      }
+      );
+
+    
+  
+  }
+
+
+ 
+
+  getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        let encoded = reader.result.toString().replace(/^data:(.*,)?/, '');
+        if ((encoded.length % 4) > 0) {
+          encoded += '='.repeat(4 - (encoded.length % 4));
+        }
+        resolve(encoded);
+      };
+      reader.onerror = error => reject(error);
+    });
+  }
+
 
 
   ngOnInit() {
@@ -192,3 +367,6 @@ public reload(): any {
 
 
 }
+
+
+
