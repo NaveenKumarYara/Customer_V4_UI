a6d2abefe0e49b9733a5eed8cd826f14f22ed3bc
Yara Naveen Kumar <yaranaveenkumar@outlook.com>
2022-09-20 17:55:56 +0530

    Added the tour package

--- src/app/components/customsearch/customsearch.component.ts
+++ src/app/components/customsearch/customsearch.component.ts
@@ -7,6 +7,7 @@ import { Subject, Observable } from 'rxjs';
 import { distinctUntilChanged, debounceTime, switchMap, tap, catchError } from 'rxjs/operators';
 import { concat } from 'rxjs/observable/concat';
 import { of } from 'rxjs/observable/of';
+import * as introJs from 'intro.js/intro.js';
 import DataSource from 'devextreme/data/data_source';
 @Component({
   selector: 'app-customsearch',
@@ -21,19 +22,54 @@ export class CustomsearchComponent implements OnInit {
   customer:any;
   userId:any;
 
-
+  introJS = introJs();
   constructor(private route: ActivatedRoute,
     private router: Router, private appService: AppService) {
       this.customer = JSON.parse(sessionStorage.getItem('userData'));
       this.userId = this.customer.UserId;
+      // this.introJS.setOptions({
+      //   steps: [
+      //     { 
+      //       intro: "Hello world!"
+      //     },
+      //     {
+      //       element: document.querySelector('#step1'),
+      //       intro: "This is a tooltip."
+      //     },
+      //     {
+      //       element: document.querySelectorAll('#step2')[0],
+      //       intro: "Ok, wasn't that fun?",
+      //       position: 'right'
+      //     },
+      //     {
+      //       element: '#step3',
+      //       intro: 'More features, more fun.',
+      //       position: 'left'
+      //     },
+      //     {
+      //       element: '#step4',
+      //       intro: "Another step.",
+      //       position: 'bottom'
+      //     },
+      //     {
+      //       element: '#step5',
+      //       intro: 'Get it, use it.'
+      //     }
+      //   ]
+      // });
 
   }
 
+  start()
+  {
+    this.introJS.start();
+  }
   ngOnInit() {
     this.dataSource = new DataSource({
       store: this.appService.getEmployees(),
     });
     this.filterValue = ['City', '=', 'Bentonville'];
+   
   }
 
 }
