17289ca8fcc10d77ff59ad3046115beba97136af
Satish <dvsvarma.work@gmail.com>
2023-04-22 18:57:58 +1000

    updated build scripts and pipelines

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 hash=`git rev-parse HEAD`
+echo $hash
+echo $BUILD_ID
 docker build . -t customer-ui:$hash
 docker tag customer-ui:$hash arytic.azurecr.io/customer-ui:$hash
+docker tag arytic.azurecr.io/customer-ui:$hash arytic.azurecr.io/customer-ui:$BUILD_ID
 echo $D_PASSWORD | docker login arytic.azurecr.io -u $D_USERNAME --password-stdin
-docker push arytic.azurecr.io/customer-ui:$hash
+docker push arytic.azurecr.io/customer-ui:$BUILD_ID
