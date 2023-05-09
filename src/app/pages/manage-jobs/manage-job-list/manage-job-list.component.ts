aa0e8f2d5c508c296c6aba7f8ab17b8a6691f771
yaranaveenkumar <yaranaveenkumar@outlook.com>
2023-05-09 15:31:57 +0530

    Added the list view Changes

--- src/app/pages/manage-jobs/manage-job-list/manage-job-list.component.ts
+++ src/app/pages/manage-jobs/manage-job-list/manage-job-list.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -5,7 +6,7 @@ import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
   selector: 'app-manage-job-list',
   templateUrl: './manage-job-list.component.html',
   standalone: true,
-  imports: [NgbCollapseModule],
+  imports: [NgbCollapseModule,CommonModule],
   styleUrls: ['./manage-job-list.component.scss']
 })
 export class ManageJobListComponent implements OnInit {
