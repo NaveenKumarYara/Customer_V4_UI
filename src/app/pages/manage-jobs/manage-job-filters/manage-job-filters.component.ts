36d66222be5c96297adccba41c1df9da50de2261
yaranaveenkumar <yaranaveenkumar@outlook.com>
2023-05-10 15:01:40 +0530

    Misc Changes

--- src/app/pages/manage-jobs/manage-job-filters/manage-job-filters.component.ts
+++ src/app/pages/manage-jobs/manage-job-filters/manage-job-filters.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { ApiService } from 'src/app/shared/components/services/api.service';
 
 @Component({
   selector: 'app-manage-job-filters',
   templateUrl: './manage-job-filters.component.html',
-  styleUrls: ['./manage-job-filters.component.scss']
+  styleUrls: ['./manage-job-filters.component.scss'],
+  providers: [ApiService]
 })
 export class ManageJobFiltersComponent implements OnInit {
   advanceFilter:boolean = false;
@@ -11,13 +13,18 @@ export class ManageJobFiltersComponent implements OnInit {
   saveSearch = false;
   @Output() newItemEvent = new EventEmitter<string>();
   @Input() filterTerm: any ='';
+  @Input() Jobs: any ='';
   @Input() viewLayout = ''; // decorate the property with @Input();
   @Output() layoutView = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private _service : ApiService) { }
   ngOnInit(): void {
   }
  
+  download(job:any){
+    debugger
+    this._service.downloadFile(job, 'MyJobs');
+  }
 
   addNewItem(value: string) {
     this.newItemEvent.emit(value);
