8aac63365fb1a60974afaf999daed075b6b3dae7
KinjalSH <kinjal02.shah@gmail.com>
2023-05-30 03:05:19 +0530

    Dashboard Page and Manage Job Grid View Responsive is Done

--- src/app/pages/dashboard/dashboard-kpis/dashboard-kpis.component.ts
+++ src/app/pages/dashboard/dashboard-kpis/dashboard-kpis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-dashboard-kpis',
@@ -6,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-kpis.component.scss']
 })
 export class DashboardKpisComponent implements OnInit {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    center:false,
+    margin: 20,
+    navText: ['<span class="mdi mdi-chevron-left"></span>', '<span class="mdi mdi-chevron-right"></span>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 5
+      }
+    },
+    nav: true
+  }
   constructor() { }
 
   ngOnInit(): void {
