7dd6ee3853ab61065b44ada919f950ab5b82dda3
KinjalSH <kinjal02.shah@gmail.com>
2023-06-24 04:25:43 +0530

    Find a candidate Desktop Theme is Done, Manage Job Quick Search UI and Responsive is Done, All 3 Slide Panel Fixes Done

--- src/app/pages/find-a-candidate/find-a-candidate.component.ts
+++ src/app/pages/find-a-candidate/find-a-candidate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-find-a-candidate',
@@ -9,12 +10,34 @@ export class FindACandidateComponent implements OnInit {
   viewLayout = 'grid';
   start:number=1;
   last:any;
+  showCardCarousel:boolean = false;
   p:number = 1;
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    center:false,
+    margin: 20,
+    items: 1,
+    navText: ['<span class="mdi mdi-chevron-left"></span>', '<span class="mdi mdi-chevron-right"></span>'],
+    nav: true
+  }
   constructor() { }
 
   ngOnInit(): void {
     this.viewLayout = 'grid';
     window.resizeBy(1,2);
+  }
+
+  showCardCarouselHandler() {
+    this.showCardCarousel = true;
+  }
+
+  showCardCarouselHideHandler() {
+    this.showCardCarousel = false;
   }
 
   listCount(count:any) {
--- src/app/pages/find-a-candidate/find-a-candidate.module.ts
+++ src/app/pages/find-a-candidate/find-a-candidate.module.ts
@@ -8,20 +8,33 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { FindACandidateGridComponent } from './find-a-candidate-grid/find-a-candidate-grid.component';
 import { FindACandidateTableComponent } from './find-a-candidate-table/find-a-candidate-table.component';
 import { FindACandidateCardComponent } from './find-a-candidate-card/find-a-candidate-card.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
 	declarations: [
-    FindACandidateComponent,
-    FindACandidateFilterComponent,
-    FindACandidateAdvanceSearchComponent,
+		FindACandidateComponent,
+		FindACandidateFilterComponent,
+		FindACandidateAdvanceSearchComponent,
 		FindACandidateGridComponent,
-  FindACandidateTableComponent,
-  FindACandidateCardComponent
+		FindACandidateTableComponent,
+		FindACandidateCardComponent
 	],
 	imports: [
-	 CommonModule,
-	 SharedModule,
-	 NgxPaginationModule
+		CommonModule,
+		SharedModule,
+		NgxPaginationModule,
+		CarouselModule,
+		NgCircleProgressModule.forRoot({
+			radius: 100,
+			outerStrokeWidth: 16,
+			innerStrokeWidth: 8,
+			outerStrokeColor: "#78C000",
+			innerStrokeColor: "#C7E596",
+			animationDuration: 300,
+			titleFontSize: '16',
+			space: -5	
+		}),
 	],
 	exports: [
     FindACandidateComponent
