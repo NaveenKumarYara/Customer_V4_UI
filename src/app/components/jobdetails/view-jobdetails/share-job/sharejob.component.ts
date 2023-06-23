1c7332dff2252bb2d933c26f5c7473abed675154
yaranaveenkumar <yaranaveenkumar@outlook.com>
2023-06-23 15:43:37 +0530

    Misc Changes

--- src/app/components/jobdetails/view-jobdetails/share-job/sharejob.component.ts
+++ src/app/components/jobdetails/view-jobdetails/share-job/sharejob.component.ts
@@ -350,7 +350,7 @@ openMailWithImage() {
     const safeImageUrl: SafeUrl = this.sanitizer.bypassSecurityTrustUrl(base64data);
 
     // Generate the mailto link
-    const mailto = `mailto:recipient@example.com?subject=Email with Image&body=Please see the attached image.%0D%0A%0D%0A`;
+    const mailto = `mailto:''?subject=Email with Image&body=Please see the attached image.%0D%0A%0D%0A`;
     const attachment = `data:image/png;base64,${encodeURIComponent(base64data)}`;
 
     // Open the mail client with the mailto link
