ba8fc085c0de67fbd1ebbfbb4d444e8971bb9fa5
yaranaveenkumar <yaranaveenkumar@outlook.com>
2023-07-12 12:48:44 +0530

    Added the Applicants list

--- src/app/pages/dashboard/dashboard-applicants/dashboard-applicants.component.ts
+++ src/app/pages/dashboard/dashboard-applicants/dashboard-applicants.component.ts
@@ -1,16 +1,25 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { ApiService } from 'src/app/shared/components/services/api.service';
+
 
 @Component({
   selector: 'app-dashboard-applicants',
   templateUrl: './dashboard-applicants.component.html',
-  styleUrls: ['./dashboard-applicants.component.scss']
+  styleUrls: ['./dashboard-applicants.component.scss',
+  '../dashboard-total-jobs/dashboard-total-jobs.component.scss'
+],
+  providers: [ApiService]
 })
 export class DashboardApplicantsComponent implements OnInit {
   cardMatching: boolean = false;
   cardID: boolean = false;
   cardExp: boolean = false;
   cardMatchingTitle: any = '';
+  customer:any;
+  topApplicants:any=[];
+  AppliedJobs:any=[];
   @Input() ProfileStats: any = ' ';
   public barChartLegend = false;
   public barChartPlugins = [];
@@ -38,9 +47,33 @@ export class DashboardApplicantsComponent implements OnInit {
   }
   public barChartType = 'bar';
 
-  constructor() { }
+  constructor(private _service : ApiService) {
+    this.customer = JSON.parse(localStorage.getItem('customer')||'');
+    this.GetCustomerTopProfiles();
+  }
 
   ngOnInit(): void {
+  }
+
+  GetCustomerTopProfiles()
+  {
+    let params = new HttpParams();
+		params = params.append("CustomerId", this.customer.CustomerId);
+    this._service.GetEmployerService("/api/GetAdminProfileHotList?", params).subscribe((response:any) => { 
+      this.topApplicants =  response;
+ 
+    });
+  }
+
+  GetCustomerJobsForApplied(ProfileId:any)
+  {
+    let params = new HttpParams();
+		params = params.append("CustomerId",this.customer.CustomerId);
+		params = params.append("ProfileId", ProfileId);
+    this._service.GetEmployerService("/api/GetCustomerJobs?", params).subscribe((response1) => { 
+
+      this.AppliedJobs = response1;
+		});
   }
 
 }
