1bf3815626613a1e3e6ecefdd6d8dd5a6b623e5f
yaranaveenkumar <yaranaveenkumar@outlook.com>
2023-07-12 13:03:18 +0530

    Misc Changes

--- src/app/pages/dashboard/dashboard-users/dashboard-users.component.ts
+++ src/app/pages/dashboard/dashboard-users/dashboard-users.component.ts
@@ -1,22 +1,37 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/shared/components/services/api.service';
 @Component({
   selector: 'app-dashboard-users',
   templateUrl: './dashboard-users.component.html',
-  styleUrls: ['./dashboard-users.component.scss']
+  styleUrls: ['./dashboard-users.component.scss'],
+  providers:[ApiService]
 })
 export class DashboardUsersComponent implements OnInit {
   cardMatching: boolean = false;
   @Input() UserStats: any ='';
+  customer:any;
+  topUsers:any=[];
   cardID: boolean = false;
   cardExp: boolean = false;
   cardMatchingTitle: any = '';
   cardClosed: boolean = false;
   cardAssigned: boolean = false;
 
-  constructor() { }
+  constructor(private _service : ApiService) {
+    this.customer = JSON.parse(localStorage.getItem('customer')||'');
+    this.GetCustomerTopUsers();
+  }
 
   ngOnInit(): void {
   }
-
+  GetCustomerTopUsers()
+  {
+    let params = new HttpParams();
+		params = params.append("CustomerId", this.customer.CustomerId);
+    this._service.GetEmployerService("/api/GetAdminUsersLists?", params).subscribe((response:any) => { 
+      this.topUsers =  response;
+ 
+    });
+  }
 }
